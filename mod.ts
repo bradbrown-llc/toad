dd28710402c398ef4be25646778ecb596c97f2e1
Brad Brown <bradbrown@bradbrown.llc>
2024-03-31 19:53:26 -0400

    if snail signal aborted, don't digest

--- mod.ts
+++ mod.ts
@@ -1,6 +1,6 @@
 import { AIQ } from 'https://deno.land/x/aiq@0.0.0/mod.ts'
 import { Lazy } from 'https://deno.land/x/lazy_promise@0.0.1/mod.ts'
-import { Snail, SomeSnail } from 'https://deno.land/x/snail@0.0.0/mod.ts'
+import { Snail, SomeSnail } from 'https://cdn.jsdelivr.net/gh/bradbrown-llc/snail@0.0.2/mod.ts'
 
 export class Toad {
 
@@ -20,16 +20,17 @@ export class Toad {
     }
 
     feed<T>(snail:Snail<T>) {
-        snail.born.then(() => this.#digest())
+        snail.born.then(() => this.#digest(snail.signal))
         const someSnail = Snail.some(snail)
         this.#someSnails.push(someSnail)
         return snail.died
     }
 
-    #digest() {
+    #digest(signal?:AbortSignal) {
+        if (signal?.aborted) return
         const promise = new Promise<void>(r => setTimeout(r, this.delay))
         const lazy:Lazy<void> = () => promise
-        const delay = new Snail(lazy)
+        const delay = new Snail({ lazy })
         const someSnail = Snail.some(delay)
         this.#someSnails.unshift(someSnail)
     }
