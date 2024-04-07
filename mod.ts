7a0315c7ac072f71c32b3cb29c010ca7fcfe32db
Brad Brown <bradbrown@bradbrown.llc>
2024-04-06 22:16:10 -0400

    update aiq and make public

--- mod.ts
+++ mod.ts
@@ -1,20 +1,20 @@
-import { AIQ } from 'https://deno.land/x/aiq@0.0.1/mod.ts'
+import { AIQ } from 'https://deno.land/x/aiq@0.0.2/mod.ts'
 import { Lazy } from 'https://deno.land/x/lazy_promise@0.0.1/mod.ts'
 import { Snail, SomeSnail } from 'https://cdn.jsdelivr.net/gh/bradbrown-llc/snail@0.0.3/mod.ts'
 
 export class Toad {
 
     delay:number
-    #someSnails:AIQ<SomeSnail>
+    aiq:AIQ<SomeSnail>
 
     constructor(delay:number) {
         this.delay = delay
-        this.#someSnails = new AIQ<SomeSnail>()
+        this.aiq = new AIQ<SomeSnail>()
         this.eat()
     }
 
     async eat() {
-        for await (const someSnail of this.#someSnails)
+        for await (const someSnail of this.aiq)
             await someSnail(<T>(snail:Snail<T>) => snail.lazy())
                 .catch(() => {})
     }
@@ -22,7 +22,7 @@ export class Toad {
     feed<T>(snail:Snail<T>) {
         snail.born.then(() => this.#digest(snail.signal))
         const someSnail = Snail.some(snail)
-        this.#someSnails.push(someSnail)
+        this.aiq.push(someSnail)
         return snail.died
     }
 
@@ -32,11 +32,7 @@ export class Toad {
         const lazy:Lazy<void> = () => promise
         const delay = new Snail({ lazy })
         const someSnail = Snail.some(delay)
-        this.#someSnails.unshift(someSnail)
-    }
-
-    get queue() {
-        return this.#someSnails
+        this.aiq.unshift(someSnail)
     }
 
 }
